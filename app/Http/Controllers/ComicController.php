4d127c7cf969f85dba82a20ca275d35c59ff4e47
SinghAmandeep <amandeepsingh3095@gmail.com>
2023-12-18 18:00:42 +0100

    add store function

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Comic;
 use App\Http\Requests\StoreComicRequest;
 use App\Http\Requests\UpdateComicRequest;
+use Illuminate\Http\Request;
 
 class ComicController extends Controller
 {
@@ -25,5 +26,24 @@ class ComicController extends Controller
     public function create()
     {
         return view('guest.create');
+    }
+
+    public function store(Request $request)
+    {
+
+        $data = $request->all();
+
+        $newComic = new Comic();
+        $newComic->description = $data['description'];
+        $newComic->title = $data['title'];
+        $newComic->thumb = $data['thumb'];
+        $newComic->price = $data['price'];
+        $newComic->series = $data['series'];
+        $newComic->sale_date = $data['sale_date'];
+        $newComic->type = $data['type'];
+
+        $newComic->save();
+
+        return redirect()->route('comics.show', $newComic->id);
     }
 }
