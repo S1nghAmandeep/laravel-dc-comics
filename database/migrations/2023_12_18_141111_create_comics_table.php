f45d45e4ab5d9b7e499f918d4b14fe7a93f768db
SinghAmandeep <amandeepsingh3095@gmail.com>
2023-12-19 18:00:35 +0100

    add decimal value to price

--- database/migrations/2023_12_18_141111_create_comics_table.php
+++ database/migrations/2023_12_18_141111_create_comics_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->string('title');
             $table->text('description');
             $table->text('thumb');
-            $table->string('price', 20);
+            $table->decimal('price');
             $table->string('series', 50);
             $table->date('sale_date');
             $table->string('type', 30);
